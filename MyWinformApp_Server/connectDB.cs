d202be05e74bba34604d4d6c14e3b48b8b0f8439
Digit24-dev <57099725+Digit24-dev@users.noreply.github.com>
2022-10-04 23:46:18 +0900

    [Server] - MySQL.Data update - things to do

--- MyWinformApp_Server/connectDB.cs
+++ MyWinformApp_Server/connectDB.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using MySql.Data;
 
 namespace MyWinformApp_Server
 {
@@ -51,6 +52,20 @@ namespace MyWinformApp_Server
             DataSet ds = new DataSet();
             da.Fill(ds);
             return ds;
+        }
+
+        /// <summary>
+        /// DB 트랜잭션 사용하기
+        /// Postgresql DB 연동 찾아보기
+        /// - Nuget 패키지 관리 > 찾아보기 탭 MySql.Data 설치
+        /// 출처 : https://dodo1054.tistory.com/114
+        /// </summary>
+        /// <param name="strSql"></param>
+        /// <returns></returns>
+        public int fn_ExecuteNonQuery(string[] strSql) 
+        {
+            
+            return 0;
         }
     }
 }
