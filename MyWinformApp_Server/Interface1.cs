d7af963c76c4c17e31b383f4328780442a022bbe
Digit24-dev <57099725+Digit24-dev@users.noreply.github.com>
2022-11-22 23:59:37 +0900

    Interface fields

---
+++ MyWinformApp_Server/Interface1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace MyWinformApp_Server
+{
+    interface IHTTP_Server
+    {
+        List<string> BindingAddressList { get; set; }
+        HttpListener httpListener { get; set; }
+        string rootPath { get; set; }
+
+        Thread Thread { get; set; }
+
+        List<Thread> responseThreadList { get; set; }
+        bool isRunning { get; set; }
+        bool isDisposed { get; set; }
+        int BUFFER_SIZE { get; set; }
+
+    }
+}
