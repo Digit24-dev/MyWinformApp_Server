3abf030fdcaf523b18de9cb8e698bf81112bfc51
Digit24-dev <57099725+Digit24-dev@users.noreply.github.com>
2022-12-06 22:15:20 +0900

    setting page

--- MyWinformApp_Server/Form1.Designer.cs
+++ MyWinformApp_Server/Form1.Designer.cs
@@ -57,6 +57,7 @@ namespace MyWinformApp_Server
             this.pictureBox1.Size = new System.Drawing.Size(430, 211);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // treeView1
             // 
@@ -112,7 +113,7 @@ namespace MyWinformApp_Server
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.Size = new System.Drawing.Size(422, 156);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -122,7 +123,7 @@ namespace MyWinformApp_Server
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(422, 156);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -132,7 +133,7 @@ namespace MyWinformApp_Server
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(192, 74);
+            this.tabPage3.Size = new System.Drawing.Size(422, 156);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -182,7 +183,7 @@ namespace MyWinformApp_Server
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "HTTP testing Form";
+            this.Text = "Setting";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
--- MyWinformApp_Server/Form1.cs
+++ MyWinformApp_Server/Form1.cs
@@ -21,5 +21,10 @@ namespace MyWinformApp_Server
         {
             
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
